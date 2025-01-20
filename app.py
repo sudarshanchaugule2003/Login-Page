8779f2fa0188d86aff0aa06ca47add23e7a12d5e
Vaibhav Achole <vaibhavachole@gmail.com>
2025-01-20 13:59:32 +0530

    CSS for templates

--- app.py
+++ app.py
@@ -267,4 +267,4 @@ def reset_password():
     return render_template("reset_password.html")
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+    app.run(debug=True, use_reloader=True, port=4000)
